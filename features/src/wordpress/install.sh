544f37e3ddd2de6d05c767e7e3fea95337cf6892
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-03-12 12:11:29 +0200

    Update wordpress feature

--- features/src/wordpress/install.sh
+++ features/src/wordpress/install.sh
@@ -12,24 +12,39 @@ fi
 echo '(*) Downloading WordPress...'
 
 if [ -z "${_REMOTE_USER}" ] || [ "${_REMOTE_USER}" = "root" ]; then
-    USER=www-data
+    WEB_USER=www-data
 else
-    USER="${_REMOTE_USER}"
+    WEB_USER="${_REMOTE_USER}"
 fi
 
 : "${VERSION:=latest}"
 
-install -d -m 0755 -o root -g root /etc/wp-cli
+install -d -m 0755 -o root -g root /etc/wp-cli /usr/share/wordpress
 install -m 0644 -o root -g root wp-cli.yaml /etc/wp-cli
-install -d -o "${USER}" -g "${USER}" -m 0755 /wp
-cp -a wp/* /wp && chown -R "${USER}:${USER}" /wp/* && chmod -R 0755 /wp/* && find /wp -type f -exec chmod 0644 {} \;
-su-exec "${USER}:${USER}" wp core download --path=/wp --skip-content --version="${VERSION}"
+install -d -o "${WEB_USER}" -g "${WEB_USER}" -m 0755 /wp
+cp -a wp/* /wp && chown -R "${WEB_USER}:${WEB_USER}" /wp/* && chmod -R 0755 /wp/* && find /wp -type f -exec chmod 0644 {} \;
+su-exec "${WEB_USER}:${WEB_USER}" wp core download --path=/wp --skip-content --version="${VERSION}"
 
 if [ "${MOVEUPLOADSTOWORKSPACES}" != 'true' ]; then
-    install -d -o "${USER}" -g "${USER}" -m 0755 /wp/wp-content/uploads
+    install -d -o "${WEB_USER}" -g "${WEB_USER}" -m 0755 /wp/wp-content/uploads
 else
-    install -d -o "${USER}" -g "${USER}" -m 0755 /workspaces/uploads
+    install -d -o "${WEB_USER}" -g "${WEB_USER}" -m 0755 /workspaces/uploads
     ln -sf /workspaces/uploads /wp/wp-content/uploads
 fi
+
+install -m 0755 -o root -g root setup-wordpress.sh /usr/local/bin/setup-wordpress.sh
+install -m 0644 -o root -g root wp-config.php.tpl wp-config-multisite.php.tpl /usr/share/wordpress/
+
+WP_DOMAIN="${DOMAIN:-localhost}"
+if [ "${MULTISITE}" != 'true' ]; then
+    WP_MULTISITE=""
+else
+    WP_MULTISITE=1
+fi
+WP_MULTISITE_TYPE="${MULTISITE_TYPE:-subdirectory}"
+
+export WP_DOMAIN WP_MULTISITE WP_MULTISITE_TYPE
+# shellcheck disable=SC2016
+envsubst '$WP_DOMAIN $WP_MULTISITE $WP_MULTISITE_TYPE' < conf-wordpress.tpl > /etc/conf.d/wordpress
 
 echo 'Done!'
