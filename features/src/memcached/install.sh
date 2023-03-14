08c1bc39c9668015f03085b5cf4714ea9b40148e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-03-14 04:31:42 +0200

    Install object cache

--- features/src/memcached/install.sh
+++ features/src/memcached/install.sh
@@ -11,9 +11,19 @@ fi
 
 if [ "${ENABLED}" = "true" ]; then
     echo '(*) Installing memcached...'
+
+    if [ -z "${_REMOTE_USER}" ] || [ "${_REMOTE_USER}" = "root" ]; then
+        PHP_USER="www-data"
+    else
+        PHP_USER="${_REMOTE_USER}"
+    fi
+
     apk add --no-cache php8-pecl-memcache php8-pecl-memcached memcached
     install -D -m 0755 -o root -g root service-run /etc/sv/memcached/run
     install -d -m 0755 -o root -g root /etc/service
+    install -d -m 0755 -o "${PHP_USER}" -g "${PHP_USER}" /wp
+    install -d -m 0755 -o "${PHP_USER}" -g "${PHP_USER}" /wp/wp-content
+    install -m 0644 -o "${PHP_USER}" -g "${PHP_USER}" object-cache.php object-cache-stable.php object-cache-next.php /wp/wp-content/
     ln -sf /etc/sv/memcached /etc/service/memcached
     echo 'Done!'
 fi
